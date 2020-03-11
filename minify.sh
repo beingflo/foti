ca3fc7f336b8b94ae32b3aa41173b44a9a0c04b1
Florian Marending <florian@marending.dev>
2020-03-11 19:37:05 +0100

    Change minify.sh

--- minify.sh
+++ minify.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-for file in $1/*
-do
-    basename=$(basename $file)
-    echo $2/$basename
-    convert $file -resize 30% $2/$basename
-    jpegoptim -m 50 $2/$basename
-done
-
-#i=1
-#for file in $1/*
-#do
-#    basename=$(basename $file)
-#    mv $1/$basename $1/$i.jpg
-#    i=$(($i+1))
-#done
-#
+convert $1 -resize 20% -quality 50 $2
